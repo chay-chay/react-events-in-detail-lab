2c9a23637f719190a03e37c96ab15da09808a12c
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-02-27 13:37:22 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,30 @@
+
+
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+export default class CoordinatesButton extends Component{
+    handleClick = (e) => {
+        this.props.onReceiveCoordinates([e.clientX, e.clientY]);
+    }
+    render(){
+        return(
+            <div>
+                <button onClick={this.handleClick}>Click</button>
+            </div>
+        )
+    }
+}
+
+// import React from 'react';
+
+// class CoordinatesButton extends React.Component {
+//   handleClick = event => {
+//     this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+//   };
+
+//   render() {
+//     return <button onClick={this.handleClick}>Coords</button>;
+//   }
+// }
+
+// export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,34 @@
 // Code DelayedButton Component Here
+ import React, {Component} from 'react';
+export default class DelayedButton extends Component{
+    handleClick = (e) => {
+        e.persist();
+    window.setTimeout(() => {
+      this.props.onDelayedClick(e);
+    }, this.props.delay);
+    }
+    render(){
+        return(
+            <div>
+                <button onClick={this.handleClick}>Delay</button>
+            </div>
+        )
+    }
+}
+
+// import React from 'react';
+
+// class DelayedButton extends React.Component {
+//   handleClick = event => {
+//     event.persist();
+//     window.setTimeout(() => {
+//       this.props.onDelayedClick(event);
+//     }, this.props.delay);
+//   };
+
+//   render() {
+//     return <button onClick={this.handleClick}>Delayed</button>;
+//   }
+// }
+
+// export default DelayedButton;
